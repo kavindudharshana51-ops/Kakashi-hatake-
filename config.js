1c88515e95a98a273cac3c2ed414aa33c5b2c6f3
kavindudharshana51-ops <kavindudharshana51@gmail.com>
2026-06-14 21:17:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "InhUVS7L#IA3je0IzQzuulamC7l6HTwv2LyLREnfGo1tVrgf-SDg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/kavindudharshana51-ops/Kakashi-hatake-/blob/main/image/lv_0_20260614003155.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 I'm kakashi hatake-MD 🥷 Is Alive Now*",
 BOT_OWNER: '94774022359',  // Replace with the owner's phone number
